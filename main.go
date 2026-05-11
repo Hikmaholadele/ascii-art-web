9e6c0417e3ac167f19f0cabbb1867d7c7f438c48
holadele <hikmaholadele14@gmail.com>
2026-05-11 10:00:28 +0100

    feat: Handled asciiHnadler function

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"text/template"
 )
@@ -15,6 +16,31 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
+func asciiHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	text := r.FormValue("text")
+	banner := r.FormValue("banner")
+
+	fmt.Println(text)
+	fmt.Println(banner)
+
+	// fmt.Fprintf(w, "Hello from server")
+	tmpl, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, "Template not found", http.StatusNotFound)
+		return
+	}
+
+	ascii := GenerateAscii(text, banner)
+	tmpl.Execute(w, ascii)
+
+}
+
 func main() {
 	http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/ascii-art", asciiHandler)
+
 }
